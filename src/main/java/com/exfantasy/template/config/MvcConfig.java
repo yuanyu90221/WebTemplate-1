379b109db044304f3cd44db1070170720cf5068a
TommyYehCool <tommy.yeh1112@gmail.com>
2016-08-24 14:27:39 +0800

    整理格式

--- src/main/java/com/exfantasy/template/config/MvcConfig.java
+++ src/main/java/com/exfantasy/template/config/MvcConfig.java
@@ -19,6 +19,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/hello").setViewName("examples/hello");
+        
         registry.addViewController("/").setViewName("examples/home");
         registry.addViewController("/home").setViewName("examples/home");
 
