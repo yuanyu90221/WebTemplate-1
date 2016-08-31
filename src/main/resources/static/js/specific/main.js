82e16dbbd68de65b696fcdb5f6b68fb90e501963
yuanyu90221 <yuanyu90221@gmail.com>
2016-08-31 17:12:45 +0800

    修改 footer位移邏輯

--- src/main/resources/static/js/specific/main.js
+++ src/main/resources/static/js/specific/main.js
@@ -3,9 +3,9 @@
  */
 $(document).ready(function(){
 	var domH= $(window).height();
-	$('.row').css('height', domH-200);
+	$('.row').css('height', domH-150);
 	$(window).resize(function(){
 		var domH= $(window).height();
-		$('.row').css('height', domH-200);
+		$('.row').css('height', domH-150);
 	});
 });
