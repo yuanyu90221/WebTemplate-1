b4fd2155471321a6680a90ac73a98cbdadef50c5
TommyYehCool <tommy.yeh1112@gmail.com>
2016-09-09 14:49:48 +0800

    移除不要的 ResultCode

--- src/main/java/com/exfantasy/template/constant/ResultCode.java
+++ src/main/java/com/exfantasy/template/constant/ResultCode.java
@@ -6,10 +6,6 @@ public enum ResultCode {
 	SUCCESS(0, "Succeed"),
 	/** 格式錯誤 */
 	INVALID_FORMAT(1001, "Invalid format"),
-	/** 查無此使用者 */
-	CANNOT_FIND_USER(1002, "Cannot find this user"),
-	/** 密碼錯誤 */
-	INCORRECT_PASSWORD(1003, "Incorrect password"),
 	/** 系統發生錯誤 */
 	SYSTEM_EXCEPTION(9999, "");
 	
