4e5decf63505c998097932bdaeb9e60ef97eb88e
TommyYehCool <tommy.yeh1112@gmail.com>
2016-09-20 11:14:35 +0800

    登出回到登入頁面寫法

--- src/main/java/com/exfantasy/template/controller/UserController.java
+++ src/main/java/com/exfantasy/template/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.exfantasy.template.constant.ResultCode;
 import com.exfantasy.template.exception.OperationException;
@@ -92,9 +91,11 @@ public class UserController {
 	}
 	
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
-	public ModelAndView logout() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("login");
-		return modelAndView;
+	public String logout() {
+//		ModelAndView modelAndView = new ModelAndView();
+//		modelAndView.setViewName("login");
+//		return modelAndView;
+		
+		return "/login";
 	}
 }
