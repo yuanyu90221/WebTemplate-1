3f34e8006cd8e2047b311755eb5186d46e676bf1
TommyYehCool <tommy.yeh1112@gmail.com>
2016-09-22 16:38:55 +0800

    完成 GenerateMyBatisCode 使用 plugin

--- src/main/java/util/GenerateMyBatisCode.java
+++ src/main/java/util/GenerateMyBatisCode.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.api.ProgressCallback;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.exception.InvalidConfigurationException;
@@ -14,36 +15,69 @@ import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 public class GenerateMyBatisCode {
-	public static void main(String[] args) {
+	private final String genCfg = System.getProperty("user.dir") + "\\mybatis-gen-config\\mybatis_generator_config.xml"; 
+	
+	private boolean overwrite = true;
+	
+	private void go() throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
 		List<String> warnings = new ArrayList<String>();  
-        boolean overwrite = true;  
-        String genCfg = System.getProperty("user.dir") + "\\mybatis-gen-config\\mybatis_generator_config.xml";
-        File configFile = new File(genCfg);  
-        ConfigurationParser cp = new ConfigurationParser(warnings);  
-        Configuration config = null;  
-        try {  
-            config = cp.parseConfiguration(configFile);  
-        } catch (IOException e) {  
-            e.printStackTrace();  
-        } catch (XMLParserException e) {  
-            e.printStackTrace();  
-        }  
-        DefaultShellCallback callback = new DefaultShellCallback(overwrite);  
-        MyBatisGenerator myBatisGenerator = null;  
-        try {  
-            myBatisGenerator = new MyBatisGenerator(config, callback, warnings);  
-        } catch (InvalidConfigurationException e) {  
-            e.printStackTrace();  
-        }  
-        try {  
-            myBatisGenerator.generate(null);  
-        } catch (SQLException e) {  
-            e.printStackTrace();  
-        } catch (IOException e) {  
-            e.printStackTrace();  
-        } catch (InterruptedException e) {  
-            e.printStackTrace();  
-        }  
 
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+
+        Configuration config = null;  
+    	File configFile = new File(genCfg);
+        config = cp.parseConfiguration(configFile);
+        
+//        System.out.println(config.toDocument().getFormattedContent());
+
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(new MyProgressCallBack());
+        
+        if (warnings.size() != 0) {
+        	System.err.println("~~~~~~ Generate failed ~~~~~~");
+        	System.err.println("Error Messages:");
+        	warnings.forEach(System.out::println);
+        }
+        else {
+        	System.out.println("------ Generate succeed ------");
+        }
+	}
+	
+	private class MyProgressCallBack implements ProgressCallback {
+
+		@Override
+		public void introspectionStarted(int totalTasks) {
+		}
+
+		@Override
+		public void generationStarted(int totalTasks) {
+			System.out.println("------ Generation started ------");
+		}
+
+		@Override
+		public void saveStarted(int totalTasks) {
+		}
+
+		@Override
+		public void startTask(String taskName) {
+		}
+
+		@Override
+		public void done() {
+			System.out.println("------ DONE ------");
+		}
+
+		@Override
+		public void checkCancel() throws InterruptedException {
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			new GenerateMyBatisCode().go();
+		} catch (IOException | XMLParserException | InvalidConfigurationException | SQLException | InterruptedException e) {
+			System.err.println("~~~~~~ Generate failed, msg: <" + e.getMessage() + ">");
+		}
 	}
 }
