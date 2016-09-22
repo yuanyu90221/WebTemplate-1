15abeae5c7c245d0b868fc1b668b97023d3c9533
TommyYehCool <tommy.yeh1112@gmail.com>
2016-09-22 14:36:54 +0800

    解決 gradle 包出來的 jar 不能跑問題

--- src/main/java/com/exfantasy/template/mybatis/mapper/UserMapper.java
+++ src/main/java/com/exfantasy/template/mybatis/mapper/UserMapper.java
@@ -1,25 +1,91 @@
 package com.exfantasy.template.mybatis.mapper;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-
 import com.exfantasy.template.mybatis.model.User;
 import com.exfantasy.template.mybatis.model.UserExample;
+import com.exfantasy.template.typehandler.BooleanTypeHandler;
+import java.util.List;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.UpdateProvider;
+import org.apache.ibatis.type.JdbcType;
 
 @Mapper
 public interface UserMapper {
+    @Delete({
+        "delete from user",
+        "where user_id = #{userId,jdbcType=INTEGER}"
+    })
     int deleteByPrimaryKey(Integer userId);
 
+    @Insert({
+        "insert into user (email, password, ",
+        "mobile_no, line_id, ",
+        "enabled, ",
+        "create_time, last_signin_time)",
+        "values (#{email,jdbcType=VARCHAR}, #{password,jdbcType=VARCHAR}, ",
+        "#{mobileNo,jdbcType=VARCHAR}, #{lineId,jdbcType=VARCHAR}, ",
+        "#{enabled,jdbcType=CHAR,typeHandler=com.exfantasy.template.typehandler.BooleanTypeHandler}, ",
+        "#{createTime,jdbcType=DATE}, #{lastSigninTime,jdbcType=TIMESTAMP})"
+    })
+    @SelectKey(statement="SELECT LAST_INSERT_ID()", keyProperty="userId", before=false, resultType=Integer.class)
     int insert(User record);
 
+    @InsertProvider(type=UserSqlProvider.class, method="insertSelective")
+    @SelectKey(statement="SELECT LAST_INSERT_ID()", keyProperty="userId", before=false, resultType=Integer.class)
     int insertSelective(User record);
 
+    @SelectProvider(type=UserSqlProvider.class, method="selectByExample")
+    @Results({
+        @Result(column="user_id", property="userId", jdbcType=JdbcType.INTEGER, id=true),
+        @Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
+        @Result(column="password", property="password", jdbcType=JdbcType.VARCHAR),
+        @Result(column="mobile_no", property="mobileNo", jdbcType=JdbcType.VARCHAR),
+        @Result(column="line_id", property="lineId", jdbcType=JdbcType.VARCHAR),
+        @Result(column="enabled", property="enabled", typeHandler=BooleanTypeHandler.class, jdbcType=JdbcType.CHAR),
+        @Result(column="create_time", property="createTime", jdbcType=JdbcType.DATE),
+        @Result(column="last_signin_time", property="lastSigninTime", jdbcType=JdbcType.TIMESTAMP)
+    })
     List<User> selectByExample(UserExample example);
 
+    @Select({
+        "select",
+        "user_id, email, password, mobile_no, line_id, enabled, create_time, last_signin_time",
+        "from user",
+        "where user_id = #{userId,jdbcType=INTEGER}"
+    })
+    @Results({
+        @Result(column="user_id", property="userId", jdbcType=JdbcType.INTEGER, id=true),
+        @Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
+        @Result(column="password", property="password", jdbcType=JdbcType.VARCHAR),
+        @Result(column="mobile_no", property="mobileNo", jdbcType=JdbcType.VARCHAR),
+        @Result(column="line_id", property="lineId", jdbcType=JdbcType.VARCHAR),
+        @Result(column="enabled", property="enabled", typeHandler=BooleanTypeHandler.class, jdbcType=JdbcType.CHAR),
+        @Result(column="create_time", property="createTime", jdbcType=JdbcType.DATE),
+        @Result(column="last_signin_time", property="lastSigninTime", jdbcType=JdbcType.TIMESTAMP)
+    })
     User selectByPrimaryKey(Integer userId);
 
+    @UpdateProvider(type=UserSqlProvider.class, method="updateByPrimaryKeySelective")
     int updateByPrimaryKeySelective(User record);
 
+    @Update({
+        "update user",
+        "set email = #{email,jdbcType=VARCHAR},",
+          "password = #{password,jdbcType=VARCHAR},",
+          "mobile_no = #{mobileNo,jdbcType=VARCHAR},",
+          "line_id = #{lineId,jdbcType=VARCHAR},",
+          "enabled = #{enabled,jdbcType=CHAR,typeHandler=com.exfantasy.template.typehandler.BooleanTypeHandler},",
+          "create_time = #{createTime,jdbcType=DATE},",
+          "last_signin_time = #{lastSigninTime,jdbcType=TIMESTAMP}",
+        "where user_id = #{userId,jdbcType=INTEGER}"
+    })
     int updateByPrimaryKey(User record);
 }
