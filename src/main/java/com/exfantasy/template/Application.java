331dee40ed5b45f84cadf9a258aac1fea93e8e10
TommyYehCool <tommy.yeh1112@gmail.com>
2016-12-09 14:22:15 +0800

    調整為原來模樣

--- src/main/java/com/exfantasy/template/Application.java
+++ src/main/java/com/exfantasy/template/Application.java
@@ -4,12 +4,8 @@ import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
 
 /**
  * <pre>
