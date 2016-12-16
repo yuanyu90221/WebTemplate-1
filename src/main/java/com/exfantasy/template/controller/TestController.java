2fb40c386e2f3bd2c013cd72fa01bc215164bbab
TommyYehCool <tommy.yeh1112@gmail.com>
2016-12-16 10:57:17 +0800

    新增測試 GET 方法

--- src/main/java/com/exfantasy/template/controller/TestController.java
+++ src/main/java/com/exfantasy/template/controller/TestController.java
@@ -5,8 +5,10 @@ import javax.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.exfantasy.template.cnst.ResultCode;
@@ -44,8 +46,8 @@ public class TestController {
 	 * @return <code>{@link com.exfantasy.template.vo.response.ResponseVo}</code> 統一回應格式
 	 */
 	@PreAuthorize("hasAuthority('" + Role.ADMIN + "')") 
-	@ApiOperation(value = "測試權限用")
 	@RequestMapping(value = "/authorities", method = RequestMethod.GET)
+	@ApiOperation(value = "測試權限用")
 	public @ResponseBody ResponseVo test() {
 		return new ResponseVo(ResultCode.SUCCESS, "Hello admin");
 	}
@@ -55,9 +57,10 @@ public class TestController {
 	 * 發送測試信件
 	 * </pre>
 	 * 
-	 * @return
+	 * @return String 若成功回傳 "Send mail succeed", 若失敗回傳 "Send mail failed, err-msg: ..."
 	 */
 	@RequestMapping(value = "/sendTestMail", method = RequestMethod.GET)
+	@ApiOperation(value = "測試發送信件")
 	public @ResponseBody String sendMail() {
 		String mailTo = "tommy.yeh1112@gmail.com";
 		String subject = "This is a test from SpringBoot";
@@ -69,5 +72,33 @@ public class TestController {
 		} catch (MessagingException e) {
 			return "Send mail failed, err-msg: " + e.getMessage();
 		}
+	}
+	
+	/**
+	 * <pre>
+	 * 測試 GET by RequestParam
+	 * </pre>
+	 * 
+	 * @param data 測試資料
+	 * @return String 回傳 "Response your data: " + data
+	 */
+	@RequestMapping(value = "/testGetByRequestParam", method = RequestMethod.GET)
+	@ApiOperation(value = "測試 GET by RequestParam")
+	public @ResponseBody String testGetByRequestParam(@RequestParam(value = "data", required = true) String data) {
+		return "Response your data: " + data;
+	}
+	
+	/**
+	 * <pre>
+	 * 測試 GET by PathVariable
+	 * </pre>
+	 * 
+	 * @param data 測試資料
+	 * @return String 回傳 "Response your data: " + data
+	 */
+	@RequestMapping(value = "/testGetByPathVariable/{data}", method = RequestMethod.GET)
+	@ApiOperation(value = "測試 GET by PathVariable")
+	public @ResponseBody String testGetByPathVariable(@PathVariable String data) {
+		return "Response your data: " + data;
 	}
 }
