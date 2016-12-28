5d068e2989d48f2d98b9195cddfe3551efcc69dd
TommyYehCool <tommy.yeh1112@gmail.com>
2016-12-28 18:09:26 +0800

    調整 swagger 註解

--- src/main/java/com/exfantasy/template/vo/response/ActivityMessagesResp.java
+++ src/main/java/com/exfantasy/template/vo/response/ActivityMessagesResp.java
@@ -2,11 +2,17 @@ package com.exfantasy.template.vo.response;
 
 import java.util.Date;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class ActivityMessagesResp {
+	@ApiModelProperty(notes = "留言者的 email", required = true)
 	private String createUserEmail;
+	
+	@ApiModelProperty(notes = "留言日期時間", required = true)
 	private Date createDatetime;
+	
+	@ApiModelProperty(notes = "留言", required = true)
 	private String msg;
 }
