a2d40c481d0f1784fafe66270aaa65544796cb60
TommyYehCool <tommy.yeh1112@gmail.com>
2017-01-26 11:44:56 +0800

    新增註解

--- src/main/java/com/exfantasy/template/cnst/CacheName.java
+++ src/main/java/com/exfantasy/template/cnst/CacheName.java
@@ -1,5 +1,13 @@
 package com.exfantasy.template.cnst;
 
+/**
+ * <pre>
+ * 定義 Redis cache name
+ * </pre>
+ * 
+ * @author tommy.feng
+ *
+ */
 public interface CacheName {
 	public static final String USER_CACHE = "userCache";
 	public static final String USER_ROLE_CACHE = "userRoleCache";
--- src/main/java/com/exfantasy/template/cnst/MailTemplateType.java
+++ src/main/java/com/exfantasy/template/cnst/MailTemplateType.java
@@ -1,5 +1,13 @@
 package com.exfantasy.template.cnst;
 
+/**
+ * <pre>
+ * 對應 Table: mail_template 的 key
+ * </pre>
+ * 
+ * @author tommy.feng
+ *
+ */
 public enum MailTemplateType {
 	consumeGotIt,
 	forgotPassword
